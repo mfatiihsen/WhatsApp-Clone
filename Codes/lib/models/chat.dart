bd91fca00b911d6837a0d0c042f6908d3afad0cc
Fatih ŞEN <ademfatih37@gmail.com>
2021-07-07 10:40:00 +0300

    WhatsApp Clone

--- Codes/lib/models/chat.dart
+++ Codes/lib/models/chat.dart
@@ -16,7 +16,8 @@ List<Chat> fakeData = [
       name: "Fatih",
       message: "Selam,Nasılsın?",
       time: "13:53",
-      avatarUrl: "sd"),
+      avatarUrl:
+          "https://img.icons8.com/color/48/000000/user-male-skin-type-6.png"),
   Chat(
       name: "Yazılımcı Genç",
       message: "Bugün buluşalım mı?",
@@ -26,5 +27,72 @@ List<Chat> fakeData = [
       name: "Patron",
       message: "İşler Bitti mi?",
       time: "21:48",
-      avatarUrl: "sd"),
+      avatarUrl:
+          "https://img.icons8.com/ios-filled/50/000000/salary-female.png"),
+  Chat(
+      name: "Emre",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-male-skin-type-3.png"),
+  Chat(
+      name: "Buse",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-female-skin-type-5.png"),
+  Chat(
+      name: "Patron",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/ios-filled/50/000000/salary-female.png"),
+  Chat(
+      name: "Emre",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-male-skin-type-3.png"),
+  Chat(
+      name: "Buse",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-female-skin-type-5.png"),
+  Chat(
+      name: "Patron",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/ios-filled/50/000000/salary-female.png"),
+  Chat(
+      name: "Emre",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-male-skin-type-3.png"),
+  Chat(
+      name: "Buse",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-female-skin-type-5.png"),
+  Chat(
+      name: "Emre",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-male-skin-type-3.png"),
+  Chat(
+      name: "Buse",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-female-skin-type-5.png"),
+  Chat(
+      name: "Emre",
+      message: "İşler Bitti mi?",
+      time: "21:48",
+      avatarUrl:
+          "https://img.icons8.com/office/16/000000/circled-user-male-skin-type-3.png"),
 ];
